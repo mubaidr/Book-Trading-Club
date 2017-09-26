4355abebc822813eecb9e9c1bb879937a61369ea
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-09-26 14:14:19 +0500

    build, publish api

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 9000,
   secret: 'minionsarecrazy',
-  //database: 'mongodb://root:root@ds143754.mlab.com:43754/book-trading-club'
-  database: 'mongodb://127.0.0.1:27017/book-trading-club'
+  database: 'mongodb://root:root@ds143754.mlab.com:43754/book-trading-club'
+  //database: 'mongodb://127.0.0.1:27017/book-trading-club'
 }
--- app/src/api.js
+++ app/src/api.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: 'http://localhost:9000',
+  url: 'https://book-store-api-mubaidr.herokuapp.com',
   books: {
     url: 'https://www.googleapis.com/books/v1/volumes?q='
   }
